080feb363570bcce48e8c87e351528cc854273e1
Sergio Arias <ariasmejiasergio13@gmail.com>
2025-03-05 11:27:23 -0500

    Se crea la animacion de cierre de cada tienda con sus productos, se comienza a estructura mejor el carrito

--- sources/js/carrito.js
+++ sources/js/carrito.js
@@ -1,7 +1,8 @@
 let contCarrito = document.getElementById('contCarrito');
 let abrirCarrito = document.getElementById('abrirCarrito');
 let cerrarCarrito = document.getElementById('cerrarCarrito');
-let imagenApertura = document.getElementById('desplegarProducto')
+let desplegarProducto = document.querySelectorAll('.desplegarProducto');
+let contInfoProducto = document.querySelectorAll('.cont-imagen-producto');
 
 abrirCarrito.addEventListener('click', () => {
     contCarrito.classList.add('visible-carrito');
@@ -12,10 +13,13 @@ cerrarCarrito.addEventListener('click', () => {
     contCarrito.classList.remove('visible-carrito');
 });
 
-imagenApertura.addEventListener('click',()=>{
-    if (imagenApertura.src.includes("../sources/img/arrow-down-circle (1).svg")) {
-        imagenApertura.src = "../sources/img/arrow-up-circle.svg"; // Nueva imagen
-    } else {
-        imagenApertura.src = "../sources/img/arrow-down-circle (1).svg"; // Imagen original
-    }
+
+desplegarProducto.forEach((producto, index) => {
+    producto.addEventListener('click', () => {
+        // Rotar la flecha
+        producto.classList.toggle('esconderProducto');
+        contInfoProducto[index].classList.toggle('oculto');
+        // Ocultar/mostrar el contenido del producto
+        
+    });
 });
