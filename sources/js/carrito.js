f599348bb68c23a61439d3cdf39b11da1e3a4480
boosam <ariasmejiasergio13@gmail.com>
2025-03-04 22:00:33 -0500

    Creamos la logica necesaria para poder en el carrito desplegar y plegar los productos

--- sources/js/carrito.js
+++ sources/js/carrito.js
@@ -1,6 +1,7 @@
 let contCarrito = document.getElementById('contCarrito');
 let abrirCarrito = document.getElementById('abrirCarrito');
 let cerrarCarrito = document.getElementById('cerrarCarrito');
+let imagenApertura = document.getElementById('desplegarProducto')
 
 abrirCarrito.addEventListener('click', () => {
     contCarrito.classList.add('visible-carrito');
@@ -9,4 +10,12 @@ abrirCarrito.addEventListener('click', () => {
 
 cerrarCarrito.addEventListener('click', () => {
     contCarrito.classList.remove('visible-carrito');
+});
+
+imagenApertura.addEventListener('click',()=>{
+    if (imagenApertura.src.includes("../sources/img/arrow-down-circle (1).svg")) {
+        imagenApertura.src = "../sources/img/arrow-up-circle.svg"; // Nueva imagen
+    } else {
+        imagenApertura.src = "../sources/img/arrow-down-circle (1).svg"; // Imagen original
+    }
 });
