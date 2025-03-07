4ac334f506f75c8f20f6b65c4636a6741f98aef3
Sergio Arias <ariasmejiasergio13@gmail.com>
2025-03-07 11:25:12 -0500

    Se termino el carrito de compras y se empieza el bentor

--- sources/js/carrito.js
+++ sources/js/carrito.js
@@ -2,15 +2,19 @@ let contCarrito = document.getElementById('contCarrito');
 let abrirCarrito = document.getElementById('abrirCarrito');
 let cerrarCarrito = document.getElementById('cerrarCarrito');
 let desplegarProducto = document.querySelectorAll('.desplegarProducto');
-let contInfoProducto = document.querySelectorAll('.cont-imagen-producto');
+let contInfoProducto = document.querySelectorAll('.cont-productos');
+let overlayCarrito = document.getElementById('overlayCarrito');
 
 abrirCarrito.addEventListener('click', () => {
+    overlayCarrito.classList.add('active-overlay-carrito');
     contCarrito.classList.add('visible-carrito');
-    
 });
 
 cerrarCarrito.addEventListener('click', () => {
+    overlayCarrito.classList.remove('active-overlay-carrito');
     contCarrito.classList.remove('visible-carrito');
+
+
 });
 
 
