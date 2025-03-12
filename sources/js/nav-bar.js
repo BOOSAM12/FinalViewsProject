2fe5d48c6563b1fb193246ec78d848c6de7311b5
Sergio Arias <ariasmejiasergio13@gmail.com>
2025-03-12 11:55:01 -0500

    Se realiza el footer

--- sources/js/nav-bar.js
+++ sources/js/nav-bar.js
@@ -1,6 +1,8 @@
 const icono_nav = document.getElementById('icono-nav-bar');
 const cont_nav = document.getElementById('cont-nav-bar');
 const close_nav = document.getElementById('close-nav-bar');
+const linkNav = document.querySelectorAll('.cont-icono');
+const contLinkNav = document.querySelectorAll('.container-links');
 
 // Función para mostrar el menú
 icono_nav.addEventListener('click', () => {
@@ -16,3 +18,17 @@ close_nav.addEventListener('click', () => {
         cont_nav.style.display = 'none';
     }, 500); 
 });
+
+//FUNCION PARA VERIFICAR A QUE LINK HACE REFERENCIA EL HOVER
+linkNav.forEach((link, index) => {
+    
+    link.addEventListener('mouseover', () => {
+        contLinkNav[index].classList.add('activeContainerLinks');
+    });
+    link.addEventListener('mouseleave', () => {
+        contLinkNav[index].classList.remove('activeContainerLinks');
+    });
+});
+
+//FUNCION PARA OCULTAR EL CONTENIDO DE LOS LINKS
+
