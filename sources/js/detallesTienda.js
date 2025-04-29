fcfbd7f7352854446ceeb38d8fe7d256cde47967
Sergio Arias <ariasmejiasergio13@gmail.com>
2025-04-29 11:10:41 -0500

    Empezamos la logica para que funcione el boton de corazon en la card del producto

--- sources/js/detallesTienda.js
+++ sources/js/detallesTienda.js
@@ -1,7 +1,14 @@
 const btnCategoria = document.querySelectorAll('.btn-categoria');
+const productos = document.querySelectorAll('.card-producto');
+const labelFavorito = document.querySelectorAll('.cont-corazon');
+
 
 btnCategoria.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         btn.classList.toggle('btn-categorias-activo');
     });
+});
+
+productos.forEach((btnFavoritoProducto, index) => {
+
 });
