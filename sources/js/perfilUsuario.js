65323a92936600b67a9029a371129734f0cfb1ce
Sergio Arias <ariasmejiasergio13@gmail.com>
2025-06-17 21:55:48 -0500

    Se hacen modificaciones a las vistas de perfil de usuario

--- sources/js/perfilUsuario.js
+++ sources/js/perfilUsuario.js
@@ -253,6 +253,26 @@ tabs.forEach(({ btn }) => {
   btn.addEventListener('click', () => activarTab(btn));
 });
 
+//MOSTRAR LA IMAGEN QUE SE SUBE EN EL INPUT DE TIPO FILE
+const fileInput = document.getElementById('imagenInput');
+const preview = document.getElementById('preview');
+
+fileInput.addEventListener('change', function () {
+  preview.innerHTML = ''; // Limpia vista previa previa
+  const files = Array.from(this.files);
+
+  files.forEach(file => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const img = document.createElement('img');
+      img.src = e.target.result;
+      img.style.width = '100px';
+      img.style.margin = '10px';
+      preview.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+  });
+});
 // Opcional: Al cargar la página, activar la primera sección o dejar todas ocultas
 // activarTab(abrirInfoBasica);
 
